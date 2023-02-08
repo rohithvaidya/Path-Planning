5ad850a89aa5ff9a8eb8f9e27bdc74ed3f754c3a
Rohith <rohithvaidya@gmail.com>
2023-02-08 19:08:36 +0530

    Updated I-DFS for all cases

--- grid_gui.py
+++ grid_gui.py
@@ -2,12 +2,19 @@ from tkinter import *
 
  
 top = Tk()  
-top.geometry("450x300") 
+top.geometry("1000x1000") 
    
 rows = 10
 cols = 10
 
-path = [0, 1, 10]
+ob = [41, 67,34,0,69,24,78,58,62,64]
+
+f=open("f.txt", "r")
+path=[]
+for i in f.readlines():
+    path.append(int(i))
+
+print(path)
 
 x=10
 y=60
@@ -15,6 +22,11 @@ ctr=0
 
 for i in range(rows):
     for j in range(cols):
+        if(ctr in ob):
+            temp = Label(top,text = str(ctr), bg= "yellow", fg= "white", textvariable=ctr,  borderwidth=1, relief="solid").place(x = x,y = y)
+            x+=20
+            ctr+=1
+            continue
         if(ctr in path):
             temp = Label(top,text = str(ctr), bg= "green", fg= "white", textvariable=ctr,  borderwidth=1, relief="solid").place(x = x,y = y)
         else:
--- main.cpp
+++ main.cpp
@@ -3,37 +3,52 @@
 #include <cstdlib>
 #include<stack>
 #include<set>
+#include <fstream>
+#define GRID_SIZE 10
+#define MAT_SIZE 100
 
 using namespace std;
 
-int adj_matrix[100][100];
+int adj_matrix[MAT_SIZE][MAT_SIZE];
+//vector<vector<int>> adj_list(GRID_SIZE,0);
 
 struct Node{
     int h=0;
     int f=0; //Total Cost
     int g = 0; //Start to Current Distance
+//Pos
+    int x;
+    int y;
+
+    struct Node* parent;
 
     int id;
     int status;
     };
 
 vector<vector<Node>> grid;
-set<int> final_path;
+vector<Node> final_path;
+set<int> dfs_path;
 
 void a_star(){
+
     vector<Node> open_list;
     vector<Node> closed_list;
     vector<Node> successors;
 
     struct Node goal_node = grid[8][8];
     goal_node.h = 0;
+    goal_node.x = 8;
+    goal_node.y = 8;
     int goal_x = 8;
     int goal_y = 8;
 
-    struct Node start_node = grid[0][0];
-    start_node.h = 98;
+    struct Node start_node = grid[0][1];
+    start_node.h = 15;
     start_node.g = 0;
-    start_node.f = 98;
+    start_node.f = 15;
+    start_node.x = 0;
+    start_node.y = 0;
 
     open_list.push_back(start_node);
 
@@ -52,18 +67,19 @@ void a_star(){
         }
     }
 
+
     closed_list.push_back(q);
     open_list.erase(open_list.begin()+q_pos);
 
 
-
-
-    for(int i=0;i<100;i++){
+    for(int i=0;i<MAT_SIZE;i++){
         if(adj_matrix[q.id][i]==1){
-            for(int j=0;j<10;j++){
-                for(int k=0;k<10;k++){
+            for(int j=0;j<GRID_SIZE;j++){
+                for(int k=0;k<GRID_SIZE;k++){
                     if(grid[j][k].id == i){
-
+                        grid[j][k].x = j;
+                        grid[j][k].y = k;
+                        grid[j][k].parent = &q;
                         successors.push_back(grid[j][k]);
                     }
                 }
@@ -72,27 +88,47 @@ void a_star(){
         }
     }
 
-
-
     for(auto i:successors){
+        if(i.id==q.id){
+            continue;
+        }
+
+        for(auto l:closed_list){
+            if((l.id == i.id)){
+                continue;
+            }
+        }
 
         if(i.id == goal_node.id){
+                cout<<"Goal Reached "<<i.id;
+                //Compute for goal node
+
+                for(int p=0;p<q.g;p++){
+                    cout<<q.parent<<" ";
+                    q = *q.parent;
+
+                }
+                i.g = q.g + 1;
+            i.h = abs(goal_x - i.x) + abs(goal_y-i.y); //Manhattan Distance
+            i.f = i.g + i.h;
+
                 cout<<endl;
-            cout<<"Goal Reached "<<i.id;
+
             cout<<"Distance "<<i.g;
             cout<<"Total Cost "<<i.f;
             cout<<endl;
 
-            for(auto o:closed_list){
-                cout<<o.id<<" ";
-            }
+
+            cout<<closed_list.size();
             return;
         }
 
         else{
+
             i.g = q.g + 1;
-            i.h = (goal_x - i.id);
+            i.h = abs(goal_x - i.x) + abs(goal_y-i.y);
             i.f = i.g + i.h;
+
         }
 
         for(auto k:open_list){
@@ -101,11 +137,7 @@ void a_star(){
             }
         }
 
-        for(auto l:closed_list){
-            if((l.id == i.id)){
-                continue;
-            }
-        }
+
         //cout<<i.id<<" ";
         open_list.push_back(i);
         }
@@ -119,72 +151,81 @@ void a_star(){
     }
 
 
+
 void dfs_with_limit(int start, int goal, vector<bool>& visited, int limit)
 {
 
-    if(limit<=1){
+ int node;
+    if(limit<0){
+        cout<<"Not found at depth";
         return;
+
     }
     stack<int> s;
-    s.push(0);
-    visited[0] = true;
+    s.push(start);
+    visited[start] = true;
 
 while(!s.empty()){
-    int node = s.top();
-    if(s.size()==1){
-        limit-=1;
-    }
+    node = s.top();
     s.pop();
-    cout<<node<<" ";
+    limit-=1;
+    dfs_path.insert(node);
     if(node==goal){
+        cout<<"Goal"<<endl;
         return;
     }
-    cout<<endl;
-    for(int i=0;i<100;i++){
-        if((!visited[i]) && (adj_matrix[node][i]==1)){
+
+    for(int i=0;i<MAT_SIZE;i++){
+        if((!visited[i]) && (adj_matrix[node][i]==1) && (limit>=0)){
             s.push(i);
             visited[i]=true;
         }
+        else{
+            continue;
+        }
     }
 }
+
+
+cout<<"Not found"<<endl;
 }
 
 
 
 void id_dfs(int start, int goal, vector<bool>& visited, int depth){
     for (int i = 1; i <= depth; i++){
-        for(int j=0; j<100; j++){
+        for(int j=0; j<MAT_SIZE; j++){
             visited[j] = false;
         }
         dfs_with_limit(start, goal, visited, i);
+
+
         cout<<endl;
     }
 
 }
 int main()
 {
-
-    int grid_length = 10; // No of elements will be NxN
-    int matrix_size = grid_length*grid_length;
     //int obstacles = (10*grid_length)/100;
-    int obstacles = 1;
+    int obstacles = 10;
 
-    vector<bool> visited(100, false);
+    vector<bool> visited(MAT_SIZE, false);
     vector<int> random_nos;
 
     vector<Node> temp;
     int ctr = 0;
 
-
+cout<<endl;
 //Randomly Assign Obstacles
 for(int i=0;i<obstacles;i++){
-    int val = rand()%matrix_size;
+    int val = rand()%MAT_SIZE;
     random_nos.push_back(val);
+    cout<<val<<" ";
 }
 
 //Initialising Grid without Obstacles
-    for(int i=0;i<grid_length;i++){
-        for(int j=0;j<grid_length;j++){
+    for(int i=0;i<GRID_SIZE;i++){
+        for(int j=0;j<GRID_SIZE;j++){
 
                 struct Node a;
                 a.id = ctr;
@@ -203,8 +244,8 @@ for(int i=0;i<obstacles;i++){
 
 
 cout<<"The Grid"<<endl;
-for(int i=0; i<grid_length; i++){
-    for(int j=0; j<grid_length; j++){
+for(int i=0; i<GRID_SIZE; i++){
+    for(int j=0; j<GRID_SIZE; j++){
         cout<<grid[i][j].status;
     }
     cout<<endl;
@@ -212,8 +253,8 @@ for(int i=0; i<grid_length; i++){
 cout<<endl;
 
 //Initialise Adj matrix
-for(int i=0; i<matrix_size; i++){
-    for(int j=0; j<matrix_size; j++){
+for(int i=0; i<MAT_SIZE; i++){
+    for(int j=0; j<MAT_SIZE; j++){
         adj_matrix[i][j] = 0;
 
 
@@ -221,9 +262,9 @@ for(int i=0; i<matrix_size; i++){
 }
 
 //Convert to Adjacency Matrix.
-for(int i=0;i<grid_length;i++){
+for(int i=0;i<GRID_SIZE;i++){
 
-    for(int j=0;j<grid_length;j++){
+    for(int j=0;j<GRID_SIZE;j++){
 
             if(grid[i][j].status){
 
@@ -233,24 +274,29 @@ for(int i=0;i<grid_length;i++){
                     int val2 = grid[i][j].id;
                     adj_matrix[val1][val2] = 1;
                     adj_matrix[val2][val1] = 1;
+                    //adj_list[val1].push_back(val2);
+                    //adj_list[val2].push_back(val1);
                 }
                 }
 
-                if((j+1)<=(grid_length-1)){
+                if((j+1)<=(GRID_SIZE-1)){
                 if(grid[i][j+1].status){
                     int val1 = grid[i][j+1].id;
                     int val2 = grid[i][j].id;
                     adj_matrix[val1][val2] = 1;
                     adj_matrix[val2][val1] = 1;
+                    //adj_list[val1].push_back(val2);
+                    //adj_list[val2].push_back(val1);
                 }
                 }
 
-                if((i+1)<=(grid_length-1)){
+                if((i+1)<=(GRID_SIZE-1)){
                     if(grid[i+1][j].status){
                     int val1 = grid[i+1][j].id;
                     int val2 = grid[i][j].id;
                     adj_matrix[val1][val2] = 1;
                     adj_matrix[val2][val1] = 1;
+
                 }
                 }
 
@@ -271,22 +317,61 @@ for(int i=0;i<grid_length;i++){
     }
 
 //Print Adjacency Matrix
-cout<<"The Adjacency Matrix"<<endl;
-for(int k=0; k<matrix_size; k++){
-    for(int l=0; l<matrix_size; l++){
+/*cout<<"The Adjacency Matrix"<<endl;
+for(int k=0; k<MAT_SIZE; k++){
+    for(int l=0; l<MAT_SIZE; l++){
         cout<<adj_matrix[k][l];
     }
     cout<<endl;
-}
+}*/
+
+cout<<"Adjacency Computed";
 
 cout<<endl;
 
 //Perform DFS
-//dfs_with_limit(0, 30, visited, 10);
+
+//dfs_with_limit(1, 88, visited, 19);
+
+id_dfs(1, 99, visited, 20);
+cout<<dfs_path.size();
+cout<<endl;
+
+
+int temp1 = 0;
+
+for(int i=0;i<GRID_SIZE;i++){
+    for(int j=0;j<GRID_SIZE;j++){
+        if(grid[i][j].status == 0){
+            cout<<"|";
+            continue;
+        }
+        for(auto k:dfs_path){
+
+            if(grid[i][j].id == k){
+                cout<<1;
+                temp1 = 1;
+            }
+        }
+
+        if(temp1 != 1){
+            cout<<0;
+        }
+        temp1 = 0;
+
+    }
+    cout<<endl;
+}
 
 //id_dfs(20, 32, visited, 10);
+//a_star();
+ofstream myfile;
+myfile.open ("E:/Course work/Foundations of AI/Path-Planning/f.txt");
+for(auto v:dfs_path){
+    myfile<<v<<endl;
+}
+myfile.close();
 
-a_star();
 
 return 0;
 
